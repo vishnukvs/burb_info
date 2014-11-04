c8375dfd9f89e098974298107a463d9d08e3055f
vishnu <vishnu.kvs@gmail.com>
2014-11-04 22:19:31 +1100

    js file

--- contact.php
+++ contact.php
@@ -42,7 +42,7 @@ require_once('dbconn.php');
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
-
+<script src="js/main.js"></script>
 
 </body>
 </html>
