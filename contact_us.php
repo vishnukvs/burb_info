065b70aaa153e7aed8a69868a1066d3be6da5ca6
vishnu <vishnu.kvs@gmail.com>
2014-11-04 22:13:15 +1100

    added co

--- contact_us.php
+++ contact_us.php
@@ -3,28 +3,28 @@ include_once('dbconn.php');
 $name = trim($_POST['name']);
 $email = trim($_POST['email']);
 $message = trim($_POST['message']);
+var_dump($name);
+
 $email_to = "vishnu@burbtech.com.au";  // This email address will recieve the data of form.html
 $email_subject = "email for Burbtech"; 
 
-function clean_text($string) {
-$bad = array("content-type","bcc:","to:","cc:","href");
-return str_replace($bad,"",$string);
-}
-
-$email_message .= " Name: ".clean_text($name)."\n";
-$email_message .= "Email: ".clean_text($email)."\n";
-$email_message .= "Comments: ".clean_text($message)."\n";
-
-@mail($email_to, $email_subject, $email_message);
-
-/*
-Put your custom message here or redirect user to any other page
-after successful submission of form.<br/>
-for example a simple message like this:<br /> */
-echo "Your form has been Submitted Successfuly.!!
-We will be back to you ASAP";
-
-}
+$email_message = "Form details below.\n\n";
+     
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+     
+    $email_message .= "Name: ".clean_string($name)."\n";
+    $email_message .= "Email: ".clean_string($email)."\n";
+    $email_message .= "Comments: ".clean_string($messgae)."\n";
+     
+     
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);  
 
 if($name&&$email!=''){
 
