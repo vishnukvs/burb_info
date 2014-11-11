13e5d79680182e203b88d361da001eb9c18dd378
vishnu <vishnu.kvs@gmail.com>
2014-11-11 22:55:57 +1100

    fixed nav bar

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,7 @@
 	</head>
 	<body>
 		<header>
-			<nav class="navbar" role="navigation">
+			<nav class="navbar navbar-default" role="navigation">
 				<div class="container-fluid">
 					<!-- Brand and toggle get grouped for better mobile display -->
 					<div class="navbar-header">
